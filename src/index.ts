285e54af8ce32aae59a997e1384b7022fcddff11
adamgoodapp <raisadamo@gmail.com>
2020-04-13 14:30:18 +0900

    Finish basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`),
+);
